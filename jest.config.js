9d46645eeeb8e25ef93278279fa1a8642b83b216
Noah lucky <luckykelimu@gmail.com>
2025-08-16 16:48:18 +0100

    Fix GitHub Actions: remove MongoDB service conflict

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,6 @@
 module.exports = {
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/tests/testConfig.js", "<rootDir>/tests/setup.js"],
-  testTimeout: 20000, // 20s to avoid random MongoDB startup delays
-  automock: false,
-  resetMocks: false,
-  clearMocks: false
+  testTimeout: 30000, // 30s to avoid random MongoDB startup delays
+  verbose: true
 };
